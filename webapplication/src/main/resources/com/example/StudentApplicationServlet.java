7f68bab7d2103a872f255be05c929a276eff9206
jyothika <jyothikakatta68@gmail.com>
2024-10-09 22:06:41 +0530

    First Commit

--- webapplication/src/main/resources/com/example/StudentApplicationServlet.java
+++
@@ -1,9 +0,0 @@
-@WebServlet("/studentApplications")
-public class StudentApplicationServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Fetch student applications from the database
-        List<Application> applications = ApplicationService.getAllApplications();
-        request.setAttribute("applications", applications);
-        request.getRequestDispatcher("/jsp/applicationTracking.jsp").forward(request, response);
-    }
-}
--- webapplication/src/main/resources/com/example/UserManagementServlet.java
+++
@@ -1,9 +0,0 @@
-@WebServlet("/userManagement")
-public class UserManagementServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Retrieve and display all users from the database
-        List<User> users = UserService.getAllUsers();
-        request.setAttribute("users", users);
-        request.getRequestDispatcher("/jsp/userManagement.jsp").forward(request, response);
-    }
-}
